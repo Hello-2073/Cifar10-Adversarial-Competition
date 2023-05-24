758031b146aeeee8bc50faa649331f5f67dd8943
肖圣鹏 <20373358@buaa.edu.cn>
2023-05-24 20:40:29 +0800

    [feat](test bench): 实现基于Resnet的分类模型

--- data/cifar10.py
+++ data/cifar10.py
@@ -1,47 +1,6 @@
 import os
 import pickle
 import numpy as np
-<<<<<<< HEAD
-import imageio
-
-
-def binary2img(root, batch, save_cls):
-    with open(os.path.join(root, 'cifar-10-batches-py', batch), 'rb') as f:
-        img_dict = pickle.load(f, encoding='bytes')
-    #  img_dict字典有4个keys
-    #    1 b'batch_label'：batch名称（文件名）
-    #    2 b'labels' ：标签
-    #    3 b'data'：图像数据
-    #    4 b'filenames'：图像名称（用这个作为文件名）
-    classification = {
-        "0": "airplane",
-        "1": "automobile",
-        "2": "bird",
-        "3": "cat",
-        "4": "deer",
-        "5": "dog",
-        "6": "frog",
-        "7": "horse",
-        "8": "ship",
-        "9": "truck",
-    }
-    for i in range(0, 10000):
-        img = np.reshape(img_dict[b'data'][i], (3, 32, 32))
-        img = img.transpose(1, 2, 0)
-        label = str(img_dict[b'labels'][i])
-        save_path = os.path.join(root, 'cifar-10-output', save_cls, str(classification.get(label)))
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        pic_name = save_path + f'\\%d.png' % i
-        imageio.imwrite(pic_name, img)
-
-
-if __name__ == '__main__':
-    root = '..\\dataset\\cifar-10-python'
-    for i in range(5):
-        binary2img(root, f'data_batch_%d' % (i + 1), 'train')
-    binary2img(root, f'test_batch', 'test')
-=======
 from PIL import Image
 
 import torch
@@ -100,4 +59,3 @@ if __name__ == '__main__':
         print("Epoch {}".format(epoch))
         for step, (imgs, labels) in enumerate(data):
             print("step {}: {}".format(step, labels))
->>>>>>> 96deaee ([feat](data): 完成完整Cifar10-python数据集的加载)
---
+++ test_bench/resnet_bench.py
@@ -0,0 +1,74 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+from .base_bench import BaseBench
+
+
+os.environ['TORCH_HOME']='D:/dataset/torchvision' 
+
+
+resnet_dict = {
+    'resnet18': 512,
+    'resnet50': 1024,
+    'resnet101': 1024,
+}
+
+
+def make_resnet(n_cls, name='resnet18'):
+    resnet = eval('torchvision.models.' + name)(pretrained=True)
+    resnet.fc = torch.nn.Linear(resnet_dict[name], n_cls)
+    return resnet
+
+
+class ResnetBench(BaseBench):
+    def __init__(self, n_cls, name, cuda=True):
+        self.name = name
+        self.resnet = make_resnet(n_cls, name)
+        if cuda:
+            self.resnet = self.resnet.cuda()
+        self.cuda = cuda
+
+    def train(self, data, iteration=200, lr=1e-3, betas=(5e-3, 5e-3), save_root='./checkpoints/'):
+        optim = torch.optim.Adam(self.resnet.fc.parameters(), lr=lr, betas=betas)
+        self.resnet.eval()
+        for epoch in range(iteration):
+            self.resnet.fc.train()
+            total_loss = 0
+            for step, (imgs, labels) in enumerate(data):
+                if self.cuda:
+                    imgs = imgs.cuda()
+                    labels = labels.cuda()
+                preds = self.resnet(imgs)
+                loss = F.cross_entropy(preds, labels)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+                total_loss += loss.item()
+            print(total_loss)
+            
+            if epoch % 5 == 0:
+                self.save(os.path.join(save_root, self.name, 'epoch{}.pth'.format(epoch)))
+
+    def test(self, data):
+        self.resnet.eval()
+        preds = []
+        labels = []
+        for img, label in data:
+            if self.cuda:
+                img = img.cuda()
+                label = label.cuda()
+            pred = self.resnet(img)
+            preds.append(F.argmax(pred))
+            labels.append(F.argmax(label))
+        confusion_matrix = F.confusion_matrix(preds, labels)
+        return preds, confusion_matrix
+
+    def save(self, path):
+        torch.save(self.resnet.fc.state_dict(), path)
+
+    def load(self, path):
+        self.resnet.fc.load_state_dict(torch.load(path))
+            
