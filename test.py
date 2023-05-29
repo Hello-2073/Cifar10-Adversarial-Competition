722ae20377c9d77747bf1eaba4d2a36ada05e38e
肖圣鹏 <20373358@buaa.edu.cn>
2023-05-29 08:54:37 +0800

    [feat](bench): 添加简单CNN的bench

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import argparse
 from torch.utils.data import DataLoader
 
 from data import Cifar10, Cifar10Clean500
-from test_bench import ResnetBench
+from test_bench import ResnetBench, CnnBench
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--bench', default='resnet18')
-    parser.add_argument('--pretrained', default=True)
+    parser.add_argument('--pretrained', default=False)
     parser.add_argument('--train_set', default='D:/Datasets/cifar-10-python/cifar-10-batches-py')
     parser.add_argument('--test_set', default='D:/Datasets/cifar10_clean_500')
     parser.add_argument('--checkpoint', default=50)
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     parser.add_argument('--cuda', default=True)
     args = parser.parse_args()
 
-    bench = ResnetBench(n_cls=10, name=args.bench, cuda=args.cuda)
+    bench = CnnBench(n_cls=10, name=args.bench, cuda=args.cuda)
 
     if not args.pretrained:
         dataset = Cifar10(root=args.train_set, train=True)
